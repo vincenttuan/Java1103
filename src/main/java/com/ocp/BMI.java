cdaa2c7b5ce4fb65615043abc4d1d4c61961cdf0
MB-teacher <MB-teacher@192.168.1.157>
2020-11-03 21:15:23 +0800

    Java

--- src/main/java/com/ocp/BMI.java
+++ src/main/java/com/ocp/BMI.java
@@ -8,7 +8,7 @@ public class BMI {
         return bmi;
     }
     double calc(int sex) {
-        double bmi = w / Math.pow(h/100, 2);
+        double bmi = calc();
         switch(sex) {
             case 1:
                 bmi *= 1.1;
