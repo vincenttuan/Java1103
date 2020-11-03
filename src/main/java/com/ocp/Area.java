611c296458dd79af0670901e7806f13c9237f367
MB-teacher <MB-teacher@192.168.1.157>
2020-11-03 20:50:25 +0800

    Java

---
+++ src/main/java/com/ocp/Area.java
@@ -0,0 +1,10 @@
+package com.ocp;
+
+public class Area {
+    int w;
+    int h;
+    void calcAndPrint(){
+        int sum = w * h;
+        System.out.println(sum);
+    }
+}
---
+++ src/main/java/com/ocp/Calc.java
@@ -0,0 +1,12 @@
+package com.ocp;
+
+public class Calc {
+    public static void main(String[] args) {
+        Area a1 = new Area();
+        a1.h = 100;
+        a1.w = 50;
+        a1.calcAndPrint();
+        
+        
+    }
+}
