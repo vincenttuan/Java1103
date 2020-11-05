c1d58b5b147c6311aaccd2a96277f00cb0945cca
MB-teacher <MB-teacher@192.168.1.157>
2020-11-05 20:43:46 +0800

    Java

--- src/main/java/com/ocp2/ATM.java
+++ src/main/java/com/ocp2/ATM.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class ATM {
     public static void main(String[] args) {
+        Account ac1 = null;
         atm_loop: // 迴圈的名字
         do {            
             // menu ---------------------------
@@ -21,21 +22,36 @@ public class ATM {
             switch(no) {
                 case 1:
                     System.out.println("建立新帳戶");
+                    ac1 = new Account();
                     break;
                 case 2:
                     System.out.println("存款");
+                    if(ac1 != null) {
+                        System.out.print("請輸入存款金額: ");
+                        int money = sc.nextInt();
+                        ac1.setBalance(money);
+                    } else {
+                        System.out.println("請先建立帳戶資料!");
+                    }
                     break;
                 case 3:
                     System.out.println("提款");
                     break;
                 case 4:
                     System.out.println("查詢");
+                    if(ac1 != null) {
+                        ac1.printBalance();
+                    } else {
+                        System.out.println("請先建立帳戶資料!");
+                    }
                     break;
                 case 9:
                     System.out.println("離開");
                     break atm_loop;
             }
-            
+            // 按下 Enter 鍵後繼續
+            System.out.println("按下 Enter 鍵後繼續 ...");
+            new Scanner(System.in).nextLine();
         } while (true);
     }
 }
