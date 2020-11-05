38c270af109ecb7f9e54a09a85d1774431e28bab
MB-teacher <MB-teacher@192.168.1.157>
2020-11-05 19:47:47 +0800

    Java

--- src/main/java/com/ocp2/Account.java
+++ src/main/java/com/ocp2/Account.java
@@ -3,7 +3,14 @@ package com.ocp2;
 // 帳戶
 public class Account {
     String name; // 戶名
-    int balance; // 餘額
+    private int balance; // 餘額
+    
+    // 存款
+    void setBalance(int money) {
+        if(money >= 0) {
+            balance += money;
+        }
+    }
     
     // 印出帳戶餘額 
     void printBalance() {
--- src/main/java/com/ocp2/Bank.java
+++ src/main/java/com/ocp2/Bank.java
@@ -5,9 +5,11 @@ public class Bank {
         Account ac1 = new Account();
         Account ac2 = new Account();
         ac1.name = "john";
-        ac1.balance = 10000;
+        ac1.setBalance(10000);
+        ac1.setBalance(5000);
         ac2.name = "mary";
-        ac2.balance = 10000;
+        ac2.setBalance(10000);
+        ac2.setBalance(3000);
         //System.out.printf("name: %s, balace: %,d\n", ac1.name, ac1.balance);
         //System.out.printf("name: %s, balace: %,d\n", ac2.name, ac2.balance);
         ac1.printBalance();
