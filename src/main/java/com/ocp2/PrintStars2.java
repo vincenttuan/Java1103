d2da7209f507b68abd9950a49791db5f7c3707d4
MB-teacher <MB-teacher@192.168.1.157>
2020-11-05 21:31:27 +0800

    Java

---
+++ src/main/java/com/ocp2/PrintStars2.java
@@ -0,0 +1,15 @@
+package com.ocp2;
+
+public class PrintStars2 {
+    public static void main(String[] args) {
+        for(int i=1;i<=6;i++) {
+            for(int k=6-i;k>0;k--) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
