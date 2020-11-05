713e0c1e157624c8cba56b9014410b6c92d8f7db
MB-teacher <MB-teacher@192.168.1.157>
2020-11-05 18:59:00 +0800

    Java

---
+++ src/main/java/com/ocp2/Account.java
@@ -0,0 +1,7 @@
+package com.ocp2;
+
+// 帳戶
+public class Account {
+    String name; // 戶名
+    int balance; // 餘額
+}
---
+++ src/main/java/com/ocp2/Bank.java
@@ -0,0 +1,14 @@
+package com.ocp2;
+
+public class Bank {
+    public static void main(String[] args) {
+        Account ac1 = new Account();
+        Account ac2 = new Account();
+        ac1.name = "john";
+        ac1.balance = 10000;
+        ac2.name = "mary";
+        ac2.balance = 10000;
+        System.out.printf("name: %s, balace: %d\n", ac1.name, ac1.balance);
+        System.out.printf("name: %s, balace: %d\n", ac2.name, ac2.balance);
+    }
+}
