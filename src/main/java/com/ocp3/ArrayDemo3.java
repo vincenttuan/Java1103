1366ad4343b1b8dd81e824aa07595d1e457adfa1
MB-teacher <MB-teacher@192.168.1.157>
2020-11-10 19:43:36 +0800

    Java

--- src/main/java/com/ocp3/ArrayDemo3.java
+++ src/main/java/com/ocp3/ArrayDemo3.java
@@ -2,7 +2,8 @@ package com.ocp3;
 
 public class ArrayDemo3 {
     public static void main(String[] args) {
-        int[] score = {100, 90, 80};
+        int[] score = {10, 9, 8};
+        score = new int[]{100, 90, 80};
         for (int i = 0; i < score.length; i++) {
             System.out.println(score[i]);
         }
