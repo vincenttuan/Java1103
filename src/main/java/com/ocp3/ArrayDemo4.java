daa3d80909a9df9e79970ad60e7fea72a2a75e00
MB-teacher <MB-teacher@192.168.1.157>
2020-11-10 21:04:53 +0800

    Java

---
+++ src/main/java/com/ocp3/ArrayDemo4.java
@@ -0,0 +1,15 @@
+package com.ocp3;
+
+/*
+    調查五位學生之身高及體重如下。
+    身高：172、168、164、170、176(公分)
+    體重：62、57、58、64、64(公斤)
+    比較其分散的程度(CV:變異係數)
+    標準差公式 : https://zh.wikipedia.org/wiki/%E6%A8%99%E6%BA%96%E5%B7%AE
+    變異係數公式 : 標準差 / 平均
+*/
+public class ArrayDemo4 {
+    public static void main(String[] args) {
+        
+    }
+}
