100a8b74bdf5b63877a27de2c3f1a53ed62f490b
MB-teacher <MB-teacher@192.168.1.157>
2020-11-10 19:50:22 +0800

    Java

--- src/main/java/com/ocp3/ArrayDemo3.java
+++ src/main/java/com/ocp3/ArrayDemo3.java
@@ -4,8 +4,15 @@ public class ArrayDemo3 {
     public static void main(String[] args) {
         int[] score = {10, 9, 8};
         score = new int[]{100, 90, 80};
+        System.out.println("列出成績:");
         for (int i = 0; i < score.length; i++) {
             System.out.println(score[i]);
         }
+        System.out.print("總分:");
+        int sum = 0;
+        for (int i = 0; i < score.length; i++) {
+            sum += score[i];
+        }
+        System.out.println(sum);
     }
 }
