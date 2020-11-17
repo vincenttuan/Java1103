dce7852db444e2952b106a340972d1e06e7f85dd
MB-teacher <MB-teacher@192.168.1.157>
2020-11-17 21:39:44 +0800

    Java

--- src/main/java/com/ocp5/MultiArray3.java
+++ src/main/java/com/ocp5/MultiArray3.java
@@ -3,9 +3,10 @@
 分別為 9 分，8 分，試問那一班的同學之程度比較平均？ 
 */
 package com.ocp5;
-
+import static com.utils.MathUtil.cv;
 public class MultiArray3 {
     public static void main(String[] args) {
-        
+        String clazz = cv(9, 80) < cv(8, 70) ? "甲班" : "乙班";
+        System.out.println(clazz);
     }
 }
