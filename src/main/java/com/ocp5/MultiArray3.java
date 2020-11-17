30448c9333891be43621b067087c685e676720fc
MB-teacher <MB-teacher@192.168.1.157>
2020-11-17 21:34:23 +0800

    Java

---
+++ src/main/java/com/ocp5/MultiArray3.java
@@ -0,0 +1,11 @@
+/*
+第一次月考，甲、乙兩班數學的平均成績分別為 80 分、70 分，標準差
+分別為 9 分，8 分，試問那一班的同學之程度比較平均？ 
+*/
+package com.ocp5;
+
+public class MultiArray3 {
+    public static void main(String[] args) {
+        
+    }
+}
