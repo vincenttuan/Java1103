c295b939c8f521c6b17ee12368299fecf8b13be3
MB-teacher <MB-teacher@192.168.1.157>
2020-11-17 19:55:01 +0800

    Java

--- src/main/java/com/ocp5/StringAndArray2.java
+++ src/main/java/com/ocp5/StringAndArray2.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class StringAndArray2 {
     public static void main(String[] args) {
-        String id = "A123456789";
+        String id = "A133334444";
         // 1 0 1 2 3 4 5 6 7 8 9 <- int[]
         char[] cs = id.toCharArray();
         System.out.println(Arrays.toString(cs));
@@ -19,5 +19,12 @@ public class StringAndArray2 {
         System.out.println(Arrays.toString(idIntArray));
         int[] delta = {1, 9, 8, 7, 6, 5, 4, 3, 2, 1, 1};
         System.out.println(Arrays.toString(delta));
+        int sum = 0;
+        for(int i=0;i<idIntArray.length;i++) {
+            sum += idIntArray[i] * delta[i];
+        }
+        System.out.println(sum);
+        boolean check = sum % 10 == 0;
+        System.out.println(check);
     }
 }
