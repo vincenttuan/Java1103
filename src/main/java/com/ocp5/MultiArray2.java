77d4d2e3d5b7e4b04f1e33d092c5a9adfcfd75dd
MB-teacher <MB-teacher@192.168.1.157>
2020-11-17 21:00:02 +0800

    Java

---
+++ src/main/java/com/ocp5/MultiArray2.java
@@ -0,0 +1,36 @@
+package com.ocp5;
+
+import java.util.Arrays;
+
+/*
+某公司有19位員工，其中10位在去年投資股票，一年的獲
+利率如下(單位：%)：
+7.6 3.9 15.6 28.3 1.2 10.8 35.3 45.6 10.2 0.5
+另外9位員工投資買公債一年內獲利率如下(單位：%)
+6.8 7.2 6.8 7.5 6.9 7.9 7.9 7.1 7.2
+試分別求此公司的員工投資股票與公債的個別平均獲利率
+試分別求此公司的員工投資股票與公債的獲利率變異係數。
+*/
+public class MultiArray2 {
+    public static void main(String[] args) {
+        double[][] profit = {
+            {7.6, 3.9, 15.6, 28.3, 1.2, 10.8, 35.3, 45.6, 10.2, 0.5},
+            {6.8, 7.2, 6.8,  7.5,  6.9, 7.9,  7.9,  7.1,  7.2},
+        };
+        for(int i=0;i<profit.length;i++) {
+            System.out.printf("%s 平均: %.1f\n", Arrays.toString(profit[i]), avg(profit[i]));
+        }
+    }
+    
+    public static double sum(double[] values) {
+        int sum = 0;
+        for(int i=0;i<values.length;i++) {
+            sum += values[i];
+        }
+        //for(int i=0;i<values.length;sum += values[i++]);
+        return sum;
+    }
+    public static double avg(double[] values) {
+        return sum(values)/values.length;
+    }
+}
