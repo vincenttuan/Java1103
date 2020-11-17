e2813efb990547ea5aa2215e85e222ed94d1a989
MB-teacher <MB-teacher@192.168.1.157>
2020-11-17 19:01:46 +0800

    Java

---
+++ src/main/java/com/ocp5/StringDemo.java
@@ -0,0 +1,12 @@
+package com.ocp5;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        String s1 = new String("Java"); // 一般 String 建立
+        String s2 = new String("Java");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2)); // 有分大小寫
+        System.out.println(s1.equalsIgnoreCase(s2)); // 不分大小寫
+        
+    }
+}
