53252c0763ef90920c8da41f8fe130391b04e7c2
MB-teacher <MB-teacher@192.168.1.157>
2020-11-19 19:12:49 +0800

    Java

---
+++ src/main/java/com/ocp6/ForLoop2.java
@@ -0,0 +1,12 @@
+package com.ocp6;
+
+public class ForLoop2 {
+    public static void main(String[] args) {
+        double[][] rows = {
+            {170.0, 180.5, 165.3},
+            {60.4, 89.5, 41.2}
+        };
+        // enhanced for loop 
+        
+    }
+}
