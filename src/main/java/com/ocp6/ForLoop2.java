ab2cf6905f8c9d41c25d9c1c8b33c4196942775d
MB-teacher <MB-teacher@192.168.1.157>
2020-11-19 19:22:10 +0800

    Java

--- src/main/java/com/ocp6/ForLoop2.java
+++ src/main/java/com/ocp6/ForLoop2.java
@@ -7,6 +7,16 @@ public class ForLoop2 {
             {60.4, 89.5, 41.2}
         };
         // enhanced for loop 
-        
+        for(double[] row : rows) {
+            for(double d : row) {
+                System.out.println(d);
+            }
+        }
+        // Standard for loop
+        for(int i=0;i<rows.length;i++) {
+            for(int j=0;j<rows[i].length;j++) {
+                System.out.println(rows[i][j]);
+            }
+        }
     }
 }
