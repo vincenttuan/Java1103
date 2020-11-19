f43ae24ff15215129ae08c64958aa9dd158390b7
MB-teacher <MB-teacher@192.168.1.157>
2020-11-19 19:34:17 +0800

    Java

--- src/main/java/com/ocp6/ForLoop3.java
+++ src/main/java/com/ocp6/ForLoop3.java
@@ -7,10 +7,26 @@ public class ForLoop3 {
             {{70, 90}, {80, 100}, {90, 90}},
         };
         // 求所有數字的總和 ?
+        // enhanced for loop
         int sum = 0;
         for (int[][] num : nums) {
             for (int[] nu : num) {
                 for (int n : nu) {
+                    System.out.println(n);
+                    sum += n;
+                }
+            }
+        }
+        System.out.printf("sum= %d\n", sum);
+        
+        // stardard for loop
+        sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int[][] num = nums[i];
+            for (int j = 0; j < num.length; j++) {
+                int[] nu = num[j];
+                for (int k = 0; k < nu.length; k++) {
+                    int n = nu[k];
                     System.out.println(n);
                     sum += n;
                 }
