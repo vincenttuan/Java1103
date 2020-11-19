930330c95edc6b9b3d232b67b918f1322203543f
MB-teacher <MB-teacher@192.168.1.157>
2020-11-19 21:16:53 +0800

    Java

--- src/main/java/com/ocp6/Mcdonalds2.java
+++ src/main/java/com/ocp6/Mcdonalds2.java
@@ -7,9 +7,13 @@ public class Mcdonalds2 {
         Hamburger h2 = new Hamburger("麥香魚", 50);
         SideMeal s1 = new SideMeal("薯條", "大", 55);
         SideMeal s2 = new SideMeal("沙拉", 40);
+        Drink d1 = new Drink("可樂", 30);
+        Drink d2 = new Drink("咖啡", 50);
+        
         // 我買了
         Hamburger[] hamburgers = {h1, h2};
         SideMeal[] sideMeals = {s1, s2};
+        Drink[] drinks = {d1, d2};
         // 總金額
         int sum = 0;
         for(Hamburger hamburger : hamburgers) {
@@ -17,6 +21,9 @@ public class Mcdonalds2 {
         }
         for(SideMeal sideMeal : sideMeals) {
             sum += sideMeal.getPrice();
+        }
+        for(Drink drink : drinks) {
+            sum += drink.getPrice();
         }
         System.out.printf("總金額: $%d\n", sum);
     }
