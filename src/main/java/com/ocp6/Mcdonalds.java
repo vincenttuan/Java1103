03aa6f6366527e5abbc1d22391fe0b63d80a1060
MB-teacher <MB-teacher@192.168.1.157>
2020-11-19 20:47:33 +0800

    Java

--- src/main/java/com/ocp6/Mcdonalds.java
+++ src/main/java/com/ocp6/Mcdonalds.java
@@ -1,5 +1,7 @@
 package com.ocp6;
 
+import java.util.Arrays;
+
 public class Mcdonalds {
     public static void main(String[] args) {
         Hamburger h = new Hamburger();
@@ -9,5 +11,14 @@ public class Mcdonalds {
         
         Hamburger h2 = new Hamburger("麥香魚", 60);
         System.out.println(h2);
-    }
+        
+        Hamburger[] hamburgers = {h, h2, h, h, h, h, h2, h2};
+        // 求總金額
+        int sum = 0;
+        for(Hamburger hamburger : hamburgers) {
+            sum += hamburger.getPrice();
+        }
+        System.out.printf("總金額 $%d\n", sum);
+        System.out.printf("你買的有: %s\n", Arrays.toString(hamburgers));
+    }   
 }
