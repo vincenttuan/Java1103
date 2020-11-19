a73d9b6338b69845208d70922e6aaa3130af2e26
MB-teacher <MB-teacher@192.168.1.157>
2020-11-19 20:58:16 +0800

    Java

---
+++ src/main/java/com/ocp6/Mcdonalds2.java
@@ -0,0 +1,16 @@
+package com.ocp6;
+
+public class Mcdonalds2 {
+    public static void main(String[] args) {
+        // 商品
+        Hamburger h1 = new Hamburger("大麥克", 70);
+        Hamburger h2 = new Hamburger("麥香魚", 50);
+        SideMeal s1 = new SideMeal("薯條", "大", 55);
+        SideMeal s2 = new SideMeal("沙拉", 40);
+        // 我買了
+        Hamburger[] hamburgers = {h1, h2};
+        SideMeal[] sideMeals = {s1, s2};
+        // 總金額
+        
+    }
+}
