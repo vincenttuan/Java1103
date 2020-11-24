5135683f8dc03fd219e77c438e38c3a566c6637c
MB-teacher <MB-teacher@192.168.1.157>
2020-11-24 19:48:17 +0800

    Java

---
+++ src/main/java/com/ocp7/Wallet.java
@@ -0,0 +1,24 @@
+package com.ocp7;
+
+public class Wallet {
+    private static String owner = "媽媽的錢包";
+    private int money = 10_0000;
+    public int getAndPrintMoney() {
+        // 資源 = 就是屬性與方法
+        // 在物件方法中可以存取物件資源與類別資源
+        System.out.println(owner + "有 $" + money);
+        return money;
+    }
+    public static void printMoney() {
+        // 在類別方法中僅能存取類別資源
+        System.out.println(owner);
+        //System.out.println(money);
+    }
+    public static void printMoney2() {
+        // 在類別方法中僅能存取類別資源
+        System.out.println(owner);
+        // 除非自行建立物件資源
+        Wallet wallet = new Wallet();
+        System.out.println(wallet.money);
+    }
+}
