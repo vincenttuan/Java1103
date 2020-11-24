3868a756e9ee607f81e681265e87efbc466dc154
MB-teacher <MB-teacher@192.168.1.157>
2020-11-24 20:38:23 +0800

    Java

---
+++ src/main/java/com/ocp7/zoo/Lion.java
@@ -0,0 +1,23 @@
+package com.ocp7.zoo;
+
+public class Lion {
+    private String name;
+    private int foot;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getFoot() {
+        return foot;
+    }
+
+    public void setFoot(int foot) {
+        this.foot = foot;
+    }
+    
+}
