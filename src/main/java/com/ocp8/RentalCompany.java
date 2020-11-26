f4a7d77ea0bfe8ebfb8070063172f65dd304dcfe
MB-teacher <MB-teacher@192.168.1.157>
2020-11-26 20:43:55 +0800

    Java

--- src/main/java/com/ocp8/RentalCompany.java
+++ src/main/java/com/ocp8/RentalCompany.java
@@ -1,13 +1,13 @@
 package com.ocp8;
 
 // 租賃工司
-
-import java.util.Arrays;
-
 public class RentalCompany {
+
     // 交通工具的庫存陣列
     private Transportation[] transportations;
-    
+    // 租金
+    private int rent;
+
     public RentalCompany() {
         // 庫存配置
         transportations = new Transportation[]{
@@ -16,10 +16,44 @@ public class RentalCompany {
             new Airplane("灣流", 5, 800, 50000),// 買飛機
         };
     }
-    
+
     // 取得交通工具庫存
     public Transportation[] getTransportations() {
         return transportations;
     }
     
+    // 取得租金
+    public int getRent() {
+        return rent;
+    }
+
+    // 租車
+    public boolean rentCar(int amount) {
+        // 查 Car 的數量 ?
+        int carAmount = 0;
+        for (Transportation ts : transportations) {
+            if (ts instanceof Car) {
+                carAmount += ts.getAmount();
+            }
+        }
+        // 是否夠租 ?
+        if (carAmount >= amount) {
+            // 租金區域變數
+            int rent = 0;
+            for (Transportation ts : transportations) {
+                if (ts instanceof Car) {
+                    // 減去庫存
+                    ts.setAmount(ts.getAmount() - amount);
+                    // 累計租金
+                    rent += ts.getPrice();
+                }
+            }
+            // 將租金區域變數的值設定給 rent 物件變數
+            this.rent = rent;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
