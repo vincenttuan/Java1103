46b9075e0263a3d516350675ac6285f3033034e7
MB-teacher <MB-teacher@192.168.1.157>
2020-11-26 21:28:41 +0800

    Java

--- src/main/java/com/ocp8/Main3.java
+++ src/main/java/com/ocp8/Main3.java
@@ -2,8 +2,17 @@ package com.ocp8;
 
 public class Main3 {
     public static void main(String[] args) {
+        int n = 2;
+        Class[] classes = {Car.class, Bicycle.class, Airplane.class};
         RentalCompany rc = new RentalCompany();
-        rc.rentTransportation(Airplane.class, 2);
+        for (Class clazz : classes) {
+            boolean success = rc.rentTransportation(clazz, n);
+            System.out.printf("租 %s %d 台 %s\n", 
+                    clazz.getSimpleName(), n, success?"成功":"失敗");
+        }
+//        rc.rentTransportation(Car.class, 2);
+//        rc.rentTransportation(Bicycle.class, 20);
+//        rc.rentTransportation(Airplane.class, 3);
         rc.print();
     }
 }
