23d80b8d26de461e541a255df0e745369d91634e
MB-teacher <MB-teacher@192.168.1.157>
2020-11-26 20:32:48 +0800

    Java

--- src/main/java/com/ocp8/Main.java
+++ src/main/java/com/ocp8/Main.java
@@ -10,6 +10,7 @@ public class Main {
         // 查看庫存
         Transportation[] trans = rc.getTransportations();
         System.out.println("目前庫存:" + Arrays.toString(trans));
+        int subtotal = 0;
         // 租一台車
         for(Transportation ts : trans) {
             if(ts instanceof Car) {
@@ -20,6 +21,7 @@ public class Main {
                 int amount = ts.getAmount();
                 amount = amount - 1;
                 ts.setAmount(amount);
+                subtotal += 1 * ts.getPrice();
             }
             if(ts instanceof Bicycle) {
                 ((Bicycle)ts).message();
@@ -27,6 +29,7 @@ public class Main {
                 int amount = ts.getAmount();
                 amount = amount - 3;
                 ts.setAmount(amount);
+                subtotal += 3 * ts.getPrice();
             }
             if(ts instanceof Airplane) {
                 ((Airplane)ts).service();
@@ -34,11 +37,12 @@ public class Main {
                 int amount = ts.getAmount();
                 amount = amount - 2;
                 ts.setAmount(amount);
+                subtotal += 2 * ts.getPrice();
             }
         }
         // 更新後的庫存資料
         System.out.println("更新後庫存:" + Arrays.toString(trans));
         // 試問總租金是 ?
-        
+        System.out.printf("總租金 $%,d\n", subtotal);
     }
 }
