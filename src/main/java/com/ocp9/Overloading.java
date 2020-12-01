8279c4583af878e1f296f6b7734710a2c58cbd25
MB-teacher <MB-teacher@192.168.1.157>
2020-12-01 19:37:36 +0800

    Java

---
+++ src/main/java/com/ocp9/Overloading.java
@@ -0,0 +1,19 @@
+package com.ocp9;
+
+public class Overloading {
+    public static void add(Integer x, Integer y) {
+        System.out.println("Integer : " + (x + y));
+    }
+    public static void add(int x, int y) {
+        System.out.println("int : " + (x + y));
+    }
+    public static void add(float x, float y) {
+        System.out.println("float : " + (x + y));
+    }
+    public static void add(double x, double y) {
+        System.out.println("double : " + (x + y));
+    }
+    public static void main(String[] args) {
+        
+    }
+}
