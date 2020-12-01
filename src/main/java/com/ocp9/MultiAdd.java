69da9607282c12f66ca3b61e4209407179677f3c
MB-teacher <MB-teacher@192.168.1.157>
2020-12-01 21:31:29 +0800

    Java

--- src/main/java/com/ocp9/MultiAdd.java
+++ src/main/java/com/ocp9/MultiAdd.java
@@ -1,13 +1,7 @@
 package com.ocp9;
 
 public class MultiAdd {
-    public static void add(int x, int y) {
-        System.out.println(x + y);
-    }
-    public static void add(int x, int y, int z) {
-        System.out.println(x + y + z);
-    }
-    public static void add(int[] nums) {
+    public static void add(int... nums) {
         int sum = 0;
         for(int n : nums) {
             sum += n;
@@ -15,11 +9,13 @@ public class MultiAdd {
         System.out.println(sum);
     }
     public static void main(String[] args) {
+        add();
+        add(10);
         add(10, 20);
         add(10, 20, 30);
-//        add(10, 20, 30, 40);
-//        add(10, 20, 30, 40, 50);
-//        add(10, 20, 30, 40, 50, 60);
+        add(10, 20, 30, 40);
+        add(10, 20, 30, 40, 50);
+        add(10, 20, 30, 40, 50, 60);
         int[] nums = {10, 20, 30, 40, 50, 60, 70};
         add(nums);
     }
