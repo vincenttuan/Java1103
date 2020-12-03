9d8d2df6dfe4ea464bfa66b6d58609a622848936
MB-teacher <MB-teacher@192.168.1.157>
2020-12-03 19:26:05 +0800

    Java

---
+++ src/main/java/com/ocp10/Android.java
@@ -0,0 +1,10 @@
+package com.ocp10;
+
+public class Android extends Mobile {
+
+    @Override
+    public String getName() {
+        return "Android 10";
+    }
+    
+}
---
+++ src/main/java/com/ocp10/IPhone.java
@@ -0,0 +1,10 @@
+package com.ocp10;
+
+public class IPhone extends Mobile {
+
+    @Override
+    public String getName() {
+        return "iPhone 12";
+    }
+    
+}
