3f9ee205211c30ef4e17aa56e09b132d03f2db1e
MB-teacher <MB-teacher@192.168.1.157>
2020-12-03 20:51:31 +0800

    Java

--- src/main/java/com/ocp10/pet/service/PetService.java
+++ src/main/java/com/ocp10/pet/service/PetService.java
@@ -1,5 +1,22 @@
 package com.ocp10.pet.service;
 
+import com.ocp10.pet.entity.Chihuahua;
+import com.ocp10.pet.entity.EDog;
+import com.ocp10.pet.entity.Pet;
+import com.ocp10.pet.entity.Shiba;
+
 public class PetService {
-    
+    // 取得目前所有的寵物(Pet), 庫存 !
+    public Pet[] getPets() {
+        Pet[] pets = {
+            new Chihuahua(15000),
+            new Chihuahua(20000),
+            new Shiba(26000),
+            new Shiba(18000),
+            new Shiba(12000),
+            new EDog(300),
+            new EDog(300),
+        };
+        return pets;
+    }
 }
