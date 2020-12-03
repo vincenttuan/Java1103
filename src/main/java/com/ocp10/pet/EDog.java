d7e852bbe3f00791aa907637e53ae92d2a10a522
MB-teacher <MB-teacher@192.168.1.157>
2020-12-03 20:41:06 +0800

    Java

---
+++ src/main/java/com/ocp10/pet/EDog.java
@@ -0,0 +1,20 @@
+package com.ocp10.pet;
+
+public class EDog extends Dog {
+    private int price;
+
+    public EDog(int price) {
+        this.price = price;
+    }
+    
+    @Override
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public void eat() {
+        System.out.println("吃3號電池2顆");
+    }
+    
+}
