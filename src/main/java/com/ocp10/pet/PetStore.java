08efcdff0ea7133e7991f7e6578992cf0026ec6f
MB-teacher <MB-teacher@192.168.1.157>
2020-12-03 21:01:52 +0800

    Java

--- src/main/java/com/ocp10/pet/PetStore.java
+++ src/main/java/com/ocp10/pet/PetStore.java
@@ -1,7 +1,14 @@
 package com.ocp10.pet;
 
+import com.ocp10.pet.entity.Pet;
+import com.ocp10.pet.service.PetService;
+
 public class PetStore {
     public static void main(String[] args) {
-        
+        PetService service = new PetService();
+        Pet[] pets = service.getPets();
+        service.print(pets);
+        int totalPrice = service.allPrice(pets);
+        System.out.printf("Total price: %,d\n", totalPrice);
     }
 }
