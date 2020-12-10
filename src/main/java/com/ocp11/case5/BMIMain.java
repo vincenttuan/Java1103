f1454a220d3a26eec5c1f9476a33fdeee3ac62f9
MB-teacher <MB-teacher@192.168.1.157>
2020-12-10 20:50:54 +0800

    Java

---
+++ src/main/java/com/ocp11/case5/BMIMain.java
@@ -0,0 +1,18 @@
+package com.ocp11.case5;
+
+public class BMIMain {
+    public static void main(String[] args) {
+        // Java 7 以前的寫法
+        BMI bmi = new BMI() {
+            @Override
+            public double getBMIByHeightAndWeight(double height, double weight) {
+                return weight / Math.pow(height/100, 2);
+            }
+        };
+        System.out.println(bmi.getBMIByHeightAndWeight(170, 60));
+        
+        // Java 8 Lambda
+        BMI bmi2 = (h, w) -> w / Math.pow(h/100, 2);
+        System.out.println(bmi2.getBMIByHeightAndWeight(170, 60));
+    }
+}
