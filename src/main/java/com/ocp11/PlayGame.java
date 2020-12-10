0c3367027e7147852459c1897a62a866169b0d59
MB-teacher <MB-teacher@192.168.1.157>
2020-12-10 18:57:39 +0800

    Java

--- src/main/java/com/ocp11/PlayGame.java
+++ src/main/java/com/ocp11/PlayGame.java
@@ -2,6 +2,8 @@ package com.ocp11;
 
 public class PlayGame {
     public static void main(String[] args) {
-        
+        Game game = new Airplane();
+        game.play();
+        System.out.println(game.price());
     }
 }
