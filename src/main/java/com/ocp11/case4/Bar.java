5c66ac407dd7d31539c23097742458affd3c0449
MB-teacher <MB-teacher@192.168.1.157>
2020-12-10 19:49:13 +0800

    Java

--- src/main/java/com/ocp11/case4/Bar.java
+++ src/main/java/com/ocp11/case4/Bar.java
@@ -1,10 +1,13 @@
 package com.ocp11.case4;
 
 public class Bar {
+    int x = 1000;
     public void hello() {
+        int x = 100;
         class Foo {
             void printMe() {
                 System.out.println("I am Foo");
+                System.out.println(x);
             }
         }
         Foo foo = new Foo();
