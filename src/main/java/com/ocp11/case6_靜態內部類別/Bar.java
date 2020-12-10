7e155dc1d7cdd1fc170fccdf89434a116e02ca7f
MB-teacher <MB-teacher@192.168.1.157>
2020-12-10 20:54:59 +0800

    Java

---
+++ src/main/java/com/ocp11/case6_靜態內部類別/Bar.java
@@ -0,0 +1,11 @@
+package com.ocp11.case6_靜態內部類別;
+
+public class Bar {
+    
+    public static class Foo {
+        public static void printMe() {
+            System.out.println("I am Foo");
+        }
+    }
+    
+}
