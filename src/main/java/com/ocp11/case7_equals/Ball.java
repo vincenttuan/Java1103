80cc7b62593404355d7296d23121fd879e86ae86
MB-teacher <MB-teacher@192.168.1.157>
2020-12-10 21:46:09 +0800

    Java

--- src/main/java/com/ocp11/case7_equals/Ball.java
+++ src/main/java/com/ocp11/case7_equals/Ball.java
@@ -1,5 +1,7 @@
 package com.ocp11.case7_equals;
 
+import java.util.Objects;
+
 public class Ball {
     private int price;
     private String color;
@@ -9,22 +11,51 @@ public class Ball {
         this.color = color;
     }
     
-    // 覆寫 equals 方法
-    @Override
-    public boolean equals(Object obj) {
-        Ball b = (Ball)obj;
-        if(b.price == price && b.color.equals(color)) {
-            return true;
-        }
-        return false;
-    }
-    
-    // 覆寫 hashcode 方法
+//    // 覆寫 equals 方法
+//    @Override
+//    public boolean equals(Object obj) {
+//        Ball b = (Ball)obj;
+//        if(b.price == price && b.color.equals(color)) {
+//            return true;
+//        }
+//        return false;
+//    }
+//    
+//    // 覆寫 hashcode 方法
+//
+//    @Override
+//    public int hashCode() {
+//        // 公式: 2 個質數 + 或 * 上有比較的屬性值
+//        return 7 * 11 * price + color.hashCode();
+//    }
 
     @Override
     public int hashCode() {
-        // 公式: 2 個質數 + 或 * 上有比較的屬性值
-        return 7 * 11 * price + color.hashCode();
+        int hash = 7;
+        hash = 79 * hash + this.price;
+        hash = 79 * hash + Objects.hashCode(this.color);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Ball other = (Ball) obj;
+        if (this.price != other.price) {
+            return false;
+        }
+        if (!Objects.equals(this.color, other.color)) {
+            return false;
+        }
+        return true;
     }
     
     
