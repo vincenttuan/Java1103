0e175374773fe8fa0616e2dc24403fc6af4ca163
MB-teacher <MB-teacher@192.168.1.157>
2020-12-10 19:43:34 +0800

    Java

---
+++ src/main/java/com/ocp11/case3/Bar.java
@@ -0,0 +1,15 @@
+package com.ocp11.case3;
+
+public class Bar {
+    int x = 1000;
+    public class Foo {
+        int x = 100;
+        public void printMe() {
+            int x = 10;
+            System.out.println("I am Foo");
+            System.out.println(x);
+            System.out.println(this.x);
+            System.out.println(Bar.this.x);
+        }
+    }
+}
---
+++ src/main/java/com/ocp11/case3/Main.java
@@ -0,0 +1,17 @@
+package com.ocp11.case3;
+
+public class Main {
+    public static void main(String[] args) {
+        // 第 1 種宣告方法:
+        //Bar.Foo foo = new Bar().new Foo();
+        //foo.printMe();
+        
+        // 第 2 種宣告方法:
+        Bar bar = new Bar();
+        Bar.Foo foo = bar.new Foo();
+        foo.printMe();
+        
+        // 第 3 種宣告方法:
+        new Bar().new Foo().printMe();
+    }
+}
