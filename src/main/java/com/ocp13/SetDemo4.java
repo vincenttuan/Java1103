5cdf69bd0d2596d90b15a9b374bc3c5edf11cc25
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-22 21:20:45 +0800

    Java

--- src/main/java/com/ocp13/SetDemo4.java
+++ src/main/java/com/ocp13/SetDemo4.java
@@ -12,15 +12,25 @@ public class SetDemo4 {
         System.out.println("全班成績: " + exams);
         System.out.println("1. 國文平均");
         // 印出所有國文成績
-        exams.stream().mapToInt(e -> e.getChinese()).forEach(score -> System.out.print(score + ", "));
+        exams.stream()
+                .mapToInt(e -> e.getChinese())
+                .forEach(score -> System.out.print(score + ", "));
         // 計算國文平均
-        double avg_chinese = exams.stream().mapToInt(e -> e.getChinese()).average().getAsDouble();
+        double avg_chinese = exams.stream()
+                .mapToInt(e -> e.getChinese())
+                .average()
+                .getAsDouble();
         System.out.println("平均: " + avg_chinese);
         System.out.println("2. 英文平均");
         // 印出所有英文成績
-        exams.stream().mapToInt(e -> e.getEnglish()).forEach(score -> System.out.print(score + ", "));
+        exams.stream()
+                .mapToInt(e -> e.getEnglish())
+                .forEach(score -> System.out.print(score + ", "));
         // 計算英文平均
-        double avg_english = exams.stream().mapToInt(e -> e.getEnglish()).average().getAsDouble();
+        double avg_english = exams.stream()
+                .mapToInt(e -> e.getEnglish())
+                .average()
+                .getAsDouble();
         System.out.println("平均: " + avg_english);
         System.out.println("3. 數學平均 (請同學自行撰寫)");
     }
