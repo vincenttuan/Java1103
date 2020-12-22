ff7836bec558bc11af4eebf8cf7a581bd8c63e5a
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-22 19:45:48 +0800

    Java

--- src/main/java/com/ocp13/SetDemo2.java
+++ src/main/java/com/ocp13/SetDemo2.java
@@ -17,6 +17,21 @@ public class SetDemo2 {
             set.add(n);
             System.out.println("加入 ==> " + n);
         }
+        
         System.out.println(set);
+        // 彩球的號碼總和 ?
+        // Java 7 for-loop
+        int sum = 0;
+        for(Object obj : set) {
+            if(obj instanceof Integer) {
+                sum += (Integer)obj;
+            }
+        }
+        System.out.println(sum);
+        // Java 8
+        int sum2 = set.stream().mapToInt(e -> ((Integer)e).intValue()).sum();
+        System.out.println(sum2);
+        int sum3 = set.stream().mapToInt(e -> (Integer)e).sum(); // Java 5 之後可以省略 intValue() 的調用
+        System.out.println(sum3);
     }
 }
