11a44f7a2ee407b4f49e34943cff7f7c1ab10111
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-22 19:29:33 +0800

    Java

--- src/main/java/com/ocp13/SetDemo2.java
+++ src/main/java/com/ocp13/SetDemo2.java
@@ -1,10 +1,20 @@
 package com.ocp13;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class SetDemo2 {
     public static void main(String[] args) {
         Set set = new HashSet();
+        // 今彩539, 1~39中取5個不重複的數字
+        // 電腦選號
+        while (set.size() < 5) {            
+            // 產生一個 1~39 的隨機數
+            int n = new Random().nextInt(39)+1;
+            set.add(n);
+            System.out.println("加入 ==> " + n);
+        }
+        System.out.println(set);
     }
 }
