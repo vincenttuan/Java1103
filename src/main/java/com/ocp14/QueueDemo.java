9ae482ab4fb80443cf3bc7a800b0f55f464cef4d
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-24 19:50:58 +0800

    Java

---
+++ src/main/java/com/ocp14/QueueDemo.java
@@ -0,0 +1,28 @@
+package com.ocp14;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class QueueDemo {
+    public static void main(String[] args) {
+        Queue<String> pokers = new LinkedList<>();
+        for (int i = 1; i <= 10; i++) pokers.add(i+"");
+        pokers.add("J");pokers.add("Q");pokers.add("K");
+        System.out.println(pokers);
+        // 洗牌
+        Collections.shuffle((List)pokers);
+        System.out.println(pokers);
+        // 拿牌
+        String card1 = pokers.poll();
+        String card2 = pokers.poll();
+        System.out.println(card1);
+        System.out.println(card2);
+        // 計算分數
+        
+        // 剩餘的牌
+        System.out.println(pokers);
+        
+    }
+}
