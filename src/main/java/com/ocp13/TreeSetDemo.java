4fa0e28076f60e20df468027ae6ec01c4cd0ca11
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-24 18:59:41 +0800

    Java

---
+++ src/main/java/com/ocp13/TreeSetDemo.java
@@ -0,0 +1,16 @@
+package com.ocp13;
+
+import com.github.javafaker.Faker;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        Set<String> names = new TreeSet();
+        Faker faker = new Faker();
+        for (int i = 0; i < 10; i++) {
+            names.add(faker.name().lastName());
+        }
+        System.out.println(names);
+    }
+}
