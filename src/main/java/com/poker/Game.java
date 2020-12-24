98926552d34105d4fd698efddf8a84b10a14b371
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-24 21:17:13 +0800

    Java

--- src/main/java/com/poker/Game.java
+++ src/main/java/com/poker/Game.java
@@ -1,6 +1,7 @@
 package com.poker;
 
 import static com.poker.PokerUtil.getPokers;
+import static com.poker.PokerUtil.getScore;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
@@ -11,14 +12,20 @@ public class Game {
     public static void main(String[] args) {
         Queue<Poker> pokers = getPokers();
         Collections.shuffle((List)pokers);
-        //System.out.println(pokers);
         play(pokers);
     }
     public static void play(Queue<Poker> pokers) {
         List<Poker> myPokers = new LinkedList<>(); // 我手邊的牌
         do {            
             Scanner sc = new Scanner(System.in);
-            //System.out.printf("你的牌是: 目前分數: 是否要牌(y/n) ? ", myPokers, );
+            System.out.printf("你的牌是: %s 目前分數: %.1f 是否要牌(y/n) ? ", myPokers, getScore(myPokers));
+            String yn = sc.next();
+            if(yn.equals("y")) {
+                Poker poker = pokers.poll();
+                myPokers.add(poker);
+            } else {
+                break;
+            }
         } while (true);
     }
 }
