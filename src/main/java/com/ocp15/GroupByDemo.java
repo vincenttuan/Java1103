1126f18100e99e7a5ddf22c16384e2ad8d1b84dc
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-29 21:18:34 +0800

    Java

--- src/main/java/com/ocp15/GroupByDemo.java
+++ src/main/java/com/ocp15/GroupByDemo.java
@@ -1,10 +1,12 @@
 package com.ocp15;
 
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import static java.util.stream.Collectors.toList;
 
 // 群組 GroupBy, 數量 Count, 排序 Sort
 public class GroupByDemo {
@@ -24,6 +26,14 @@ public class GroupByDemo {
         result.entrySet().stream()
                          .sorted((a, b) -> (int)(b.getValue() - a.getValue()))
                          .forEach(e -> System.out.println(e));
+        
+        // 將排序結果放入到集合
+        Map map = new LinkedHashMap();
+        result.entrySet().stream()
+                         .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                         .forEach(e -> map.put(e.getKey(), e.getValue()));
+        System.out.println(map);
+        
         
         
     }
