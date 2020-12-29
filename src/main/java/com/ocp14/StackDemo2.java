777a41048b4f459c9b5bd2bc51f115a99b6f02a4
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-29 19:30:38 +0800

    Java

---
+++ src/main/java/com/ocp14/StackDemo2.java
@@ -0,0 +1,21 @@
+package com.ocp14;
+
+// 利用堆疊實作字串反轉並印出
+
+import java.util.Stack;
+
+public class StackDemo2 {
+    public static void main(String[] args) {
+        String str = "Java";
+        // 將每一個字放入到堆疊
+        Stack<Character> stack = new Stack<>();
+        for(int i=0;i<str.length();i++) {
+            stack.add(str.charAt(i));
+        }
+        System.out.println(stack);
+        System.out.print("反轉: ");
+        while (!stack.isEmpty()) {            
+            System.out.print(stack.pop());
+        }
+    }
+}
