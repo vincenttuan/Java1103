0dccdc3d45075ce05bba311f77d3b21b1daae5b1
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-29 20:38:53 +0800

    Java

--- src/main/java/com/ocp14/MapDemo.java
+++ src/main/java/com/ocp14/MapDemo.java
@@ -14,9 +14,13 @@ public class MapDemo {
         System.out.println(map.get("英"));
         System.out.println(map.keySet()); // 取出所有的 key
         System.out.println(map.values()); // 取出所有的 value
-        
         for(String key : map.keySet()) {
             System.out.println(key + " 分數:" + map.get(key));
         }
+        // Java 8
+        map.entrySet().forEach(e -> System.out.println(e.getKey() + " = " + e.getValue()));
+        // 計算總分
+        int sum = map.entrySet().stream().mapToInt(e -> e.getValue()).sum();
+        System.out.println(sum);
     }
 }
