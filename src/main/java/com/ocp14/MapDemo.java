be3f6f036070682681592538c286007d3ddc5484
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2020-12-29 20:06:02 +0800

    Java

---
+++ src/main/java/com/ocp14/MapDemo.java
@@ -0,0 +1,22 @@
+package com.ocp14;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        map.put("國", 100);
+        map.put("英", 90);
+        map.put("數", 100);
+        System.out.println(map);
+        System.out.println(map.get("英"));
+        System.out.println(map.keySet()); // 取出所有的 key
+        System.out.println(map.values()); // 取出所有的 value
+        
+        for(String key : map.keySet()) {
+            System.out.println(key + " 分數:" + map.get(key));
+        }
+    }
+}
