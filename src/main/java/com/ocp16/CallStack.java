ba31b9c118cba786796bc6694eb4691c7c52cc7b
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-05 20:45:43 +0800

    Java

---
+++ src/main/java/com/ocp16/CallStack.java
@@ -0,0 +1,24 @@
+package com.ocp16;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class CallStack {
+    public static void main(String[] args) {
+        try {
+            calc();
+        } catch (FileNotFoundException ex) {
+            System.out.println("main方法處理了此問題: " + ex);
+        }
+    }
+    public static void calc() throws FileNotFoundException {
+        openFile();
+    }
+    public static void openFile() throws FileNotFoundException {
+        File file = new File("src\\main\\java\\com\\ocp16\\number2.txt");
+        Scanner sc = new Scanner(file);
+    }
+}
---
+++ src/main/java/com/ocp16/ExceptionDemo3.java
@@ -0,0 +1,20 @@
+package com.ocp16;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ExceptionDemo3 {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src\\main\\java\\com\\ocp16\\number2.txt");
+//        try {
+//            Scanner sc = new Scanner(file);
+//            System.out.println("找到檔案了");
+//        } catch (FileNotFoundException e) {
+//            System.out.println("找不到檔案");
+//        }
+        Scanner sc = new Scanner(file);
+        System.out.println("找到檔案了");
+    }
+}
