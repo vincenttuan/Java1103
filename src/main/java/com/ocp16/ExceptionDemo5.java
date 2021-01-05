bff3788ba937804e7491f3dd5c055568f80acddf
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-05 21:16:49 +0800

    Java

---
+++ src/main/java/com/ocp16/ExceptionDemo5.java
@@ -0,0 +1,32 @@
+package com.ocp16;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ExceptionDemo5 {
+    public static void main(String[] args) {
+        try {
+            System.out.println(calcBMI(170, 60));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        try {
+            System.out.println(calcBMI(-170, 60));
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        
+    }
+    
+    public static double calcBMI(double h, double w) throws Exception {
+        if(h <= 0) {
+            Exception e = new Exception("身高不可 <= 0");
+            throw e;
+        }
+        if(w <= 0) {
+            Exception e = new Exception("體重不可 <= 0");
+            throw e;
+        }
+        return w / Math.pow(h/100, 2);
+    }
+}
