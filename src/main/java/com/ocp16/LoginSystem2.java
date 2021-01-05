6f6582224d11418e26a48b139e9a43d015e00a1a
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-05 21:35:20 +0800

    Java

---
+++ src/main/java/com/ocp16/LoginSystem2.java
@@ -0,0 +1,30 @@
+package com.ocp16;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoginSystem2 {
+    // 可以登入的使用者
+    private static List<String> users = Arrays.asList("john", "mary", "admin");
+    
+    public static boolean login(String userName) throws LoginException {
+        boolean check = users.stream().filter(name -> name.equals(userName)).findFirst().isPresent();
+        if(check == false) { // if(!check)
+            LoginException le = new LoginException("無此帳號");
+            throw le;
+        }
+        return check;
+    }
+    
+    public static void main(String[] args) {
+        try {
+            System.out.println(login("apple"));
+        } catch (LoginException ex) {
+            System.out.println(ex.getMessage());
+            ex.怎麼辦();
+        }
+        
+    }
+}
