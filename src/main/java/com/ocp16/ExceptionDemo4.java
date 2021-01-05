4d54673543499cf21c3757415207fab45a4e7972
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-05 21:09:50 +0800

    Java

---
+++ src/main/java/com/ocp16/ExceptionDemo4.java
@@ -0,0 +1,16 @@
+package com.ocp16;
+
+public class ExceptionDemo4 {
+    public static void main(String[] args) {
+        // foo(); // 必須使用 try-catch 或 throws 拋出
+        // bar(); // 直接使用即可
+    }
+    
+    public static void foo() throws Exception { // checked exception
+        System.out.println("foo");
+    }
+    
+    public static void bar() throws RuntimeException { // Unchecked exception
+        System.out.println("bar");
+    }
+}
