c46ef9c958d52d0fd4f983910254464d49dfe3a1
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-07 19:40:04 +0800

    Java

--- src/main/java/com/ocp16_exception/Square.java
+++ src/main/java/com/ocp16_exception/Square.java
@@ -3,7 +3,8 @@ package com.ocp16_exception;
 // 計算面積的類別
 public class Square {
     public int getArea(int w, int h) {
-        int area = w + h;
+        //int area = w + h; // v1.0
+        int area = w * h; // v2.0
         return area;
     }
 }
--- src/test/java/ocp16/Test_Square.java
+++ src/test/java/ocp16/Test_Square.java
@@ -1,5 +1,7 @@
 package ocp16;
 
+import com.ocp16_exception.Square;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class Test_Square {
@@ -9,6 +11,18 @@ public class Test_Square {
         // Arrange - 安排(目標物件)
         // Act - 目標方法
         // Assert - 驗證是否符合預期 ?
+        
+        // Arrange - 安排(目標物件)
+        Square sq = new Square();
+        // 參數:
+        int w = 10;
+        int h = 5;
+        // 期望結果
+        int exp = 50; // exp 表示期望值
+        // Act - 目標方法
+        int act = sq.getArea(w, h); // act 表示實際值
+        // Assert - 驗證是否符合預期 ?
+        Assert.assertEquals(exp, act); // exp 期望值 == act 實際值
         
     }
 }
