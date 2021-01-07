43dcf3b703dd2903b32e4ccffd7111b8bd61f486
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-07 21:43:44 +0800

    Java

--- src/main/java/com/pattern/observer/Test.java
+++ src/main/java/com/pattern/observer/Test.java
@@ -3,6 +3,22 @@ package com.pattern.observer;
 public class Test {
     public static void main(String[] args) {
         Subject news = new News();
+        Observer user1 = new User("小明");
+        Observer user2 = new User("小華");
+        Observer user3 = new User("小英");
+        // 訂閱
+        news.add(user1);
+        news.add(user2);
+        news.add(user3);
+        // 發送新聞
+        news.notifyObserver("最近天氣很冷~~~");
+        System.out.println();
+        // 退訂
+        news.remove(user2);        
+        // 發送新聞
+        news.notifyObserver("明天下雪~~~");
+        System.out.println();
         
     }
 }
+
