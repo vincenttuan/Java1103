268b163481f13a9a3a382762f5aedb83ab4852b7
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-07 19:31:11 +0800

    Java

---
+++ src/test/java/ocp16/Test_Square.java
@@ -0,0 +1,14 @@
+package ocp16;
+
+import org.junit.Test;
+
+public class Test_Square {
+    @Test
+    public void test() {
+        // 測試 3A
+        // Arrange - 安排(目標物件)
+        // Act - 目標方法
+        // Assert - 驗證是否符合預期 ?
+        
+    }
+}
