59480bd66aa933607fd7b93b63b8bc58f713b0c5
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-07 21:32:50 +0800

    Java

--- src/main/java/com/pattern/observer/News.java
+++ src/main/java/com/pattern/observer/News.java
@@ -1,9 +1,23 @@
 package com.pattern.observer;
 
-/**
- *
- * @author MB-teacher
- */
-public class News {
+import java.util.ArrayList;
+import java.util.List;
+
+public class News implements Subject {
+    private List<Observer> observers = new ArrayList<>(); // 我的觀察者列表(粉絲人數)
+    @Override
+    public void add(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void remove(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObserver(String data) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
