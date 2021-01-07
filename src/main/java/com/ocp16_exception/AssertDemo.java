1ab275283f86402fa81dab3b246993e4524c4b87
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-07 19:09:04 +0800

    Java

---
+++ src/main/java/com/ocp16_exception/AssertDemo.java
@@ -0,0 +1,12 @@
+package com.ocp16_exception;
+
+import java.util.Random;
+
+public class AssertDemo {
+    public static void main(String[] args) {
+        int x = new Random().nextInt(100);
+        // 假設 x 都要應該大於等於 >= 60
+        assert (x >= 60) : "發生 Assert 錯誤, 挑戰失敗, x = " + x;
+        System.out.printf("%d 大於等於 >= 60 正確\n", x);
+    }
+}
