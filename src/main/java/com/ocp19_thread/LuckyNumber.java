b9d15f3eadbaf65c8f04702d91dc022650645a7a
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-14 19:09:17 +0800

    Java

--- src/main/java/com/ocp19_thread/LuckyNumber.java
+++ src/main/java/com/ocp19_thread/LuckyNumber.java
@@ -1,5 +1,16 @@
 package com.ocp19_thread;
 
 public class LuckyNumber extends Thread {
+    private static final int LUCKY_NUMBER = 777;
+    @Override
+    public void run() {
+        String name = Thread.currentThread().getName();
+        for (int i = 1;true; i++) {
+            int number = (int)Math.random() * 1000;
+            if(number == LUCKY_NUMBER) {
+                System.out.printf("%s 總共取了 %d 次取到 %d\n", name, i, LUCKY_NUMBER);
+            }
+        }
+    }
     
 }
