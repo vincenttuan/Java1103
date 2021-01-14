4c9c826c65752e6df9be3c79107562451fdbd0c0
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-14 20:52:33 +0800

    Java

---
+++ src/main/java/com/ocp19_thread/Shower.java
@@ -0,0 +1,40 @@
+package com.ocp19_thread;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class Father extends Thread {
+    @Override
+    public void run() {
+        System.out.println("爸爸下班回家");
+        System.out.println("爸爸準備洗澡");
+        System.out.println("爸爸發現沒瓦斯");
+        System.out.println("爸爸打電話叫瓦斯");
+        Worker worker = new Worker();
+        worker.start();
+    }
+}
+
+class Worker extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println("工人送瓦斯中...");
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + "秒鐘  ");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) { // 中斷例外
+                System.out.println("瓦斯工人發生意外");
+                return;
+            }
+        }
+        System.out.println();
+        System.out.println("工人將瓦斯送到");
+    }
+    
+}
+
+public class Shower {
+    
+}
