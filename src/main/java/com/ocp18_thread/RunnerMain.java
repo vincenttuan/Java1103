c01bdaebefa4d2fd7741e31f069a518accca2e61
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-14 20:02:12 +0800

    Java

--- src/main/java/com/ocp18_thread/RunnerMain.java
+++ src/main/java/com/ocp18_thread/RunnerMain.java
@@ -6,9 +6,12 @@ public class RunnerMain {
         Runner r2 = new Runner();
         r1.setName("烏龜");
         r2.setName("兔子");
-        r1.setPriority(Thread.MAX_PRIORITY); // 1~10
-        r2.setPriority(Thread.MIN_PRIORITY); // 1~10
+        // 背景執行緒
+        r2.setDaemon(true);
+        //r1.setPriority(Thread.MAX_PRIORITY); // 1~10
+        //r2.setPriority(Thread.MIN_PRIORITY); // 1~10
         r1.start(); // 啟動執行緒 
         r2.start(); // 啟動執行緒
+        
     }
 }
