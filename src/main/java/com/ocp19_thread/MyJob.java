28907a78dbf87078097b790a90926ba35098f4ba
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-14 19:52:46 +0800

    Java

--- src/main/java/com/ocp19_thread/MyJob.java
+++ src/main/java/com/ocp19_thread/MyJob.java
@@ -5,7 +5,8 @@ public class MyJob {
         Runnable r1 = new Job1();
         Runnable r2 = new Job2();
         Runnable r3 = new Job3();
-        Thread t = new Thread(r3);
+        Runnable r4 = () -> System.out.println("買早餐");
+        Thread t = new Thread(r4);
         t.start();
     }
 }
