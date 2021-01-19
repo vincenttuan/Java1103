70a312eaaeea6af31172f6c7352f71abc45c7814
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-19 20:46:40 +0800

    Java

--- src/main/java/com/ocp20_thread/ExamDemo.java
+++ src/main/java/com/ocp20_thread/ExamDemo.java
@@ -1,6 +1,10 @@
 package com.ocp20_thread;
 
 import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.FutureTask;
 
 class Runner implements Runnable {
     @Override
@@ -18,5 +22,15 @@ class Caller implements Callable<String> {
 }
 
 public class ExamDemo {
-    
+    public static void main(String[] args) throws Exception {
+        Runner runner = new Runner();
+        Caller caller = new Caller();
+        ExecutorService es = Executors.newFixedThreadPool(2);
+        Future f1 = es.submit(runner);
+        Future f2 = es.submit(caller);
+        Object s1 = f1.get();
+        Object s2 = f2.get();
+        System.out.println(s1);
+        System.out.println(s2);
+    }
 }
