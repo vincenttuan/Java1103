4922f8bf64491ec8866d933041dbab3a8076e2f1
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-21 19:23:25 +0800

    Java

--- src/main/java/com/ocp22_wait_notify/Cookie.java
+++ src/main/java/com/ocp22_wait_notify/Cookie.java
@@ -1,17 +1,38 @@
 package com.ocp22_wait_notify;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Cookie {
     
+    private boolean empty = true; // 盤子的狀態
+    
     // 吃餅乾
-    public void eat(int no) {
+    public synchronized void eat(int no) {
+        if(empty) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+            }
+        }
         String name = Thread.currentThread().getName();
         System.out.printf("%s 吃了第 %d 塊餅乾\n", name, no);
+        empty = true;
+        notify();
     }
     
     // 放餅乾
-    public void put(int no) {
+    public synchronized void put(int no) {
+        if(!empty) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+            }
+        }
         String name = Thread.currentThread().getName();
         System.out.printf("%s 放了第 %d 塊餅乾\n", name, no);
+        empty = false;
+        notify();
     }
     
 }
