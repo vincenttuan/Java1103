f4e0176376243993c38500f5d361b89590a22d09
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-01-21 20:39:27 +0800

    Java

--- src/main/java/com/ocp23_cyclicbarrier/Car.java
+++ src/main/java/com/ocp23_cyclicbarrier/Car.java
@@ -2,6 +2,7 @@ package com.ocp23_cyclicbarrier;
 
 import java.util.Random;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
 
 public class Car extends Thread {
     private CyclicBarrier cb;
@@ -17,10 +18,11 @@ public class Car extends Thread {
         try {
             Thread.sleep(new Random().nextInt(5000));
             System.out.printf("%s 到台中了\n", name);
-            cb.await(); // 等待其他人
+            cb.await(10, TimeUnit.SECONDS); // 等待其他人
             System.out.printf("%s 繼續往高雄前進\n", name);
             Thread.sleep(new Random().nextInt(5000));
         } catch (Exception e) {
+            System.out.printf("%s 等不耐了 ! %s\n", name, e.toString());
         }
         System.out.printf("%s 到高雄了\n", name);
     }
--- src/main/java/com/ocp23_cyclicbarrier/ReturnHome.java
+++ src/main/java/com/ocp23_cyclicbarrier/ReturnHome.java
@@ -7,7 +7,7 @@ public class ReturnHome {
         int n = 3;
         Runnable lunch = () -> System.out.println("吃午餐~~");
         CyclicBarrier cb = new CyclicBarrier(n, lunch);
-        for(int i=1;i<=3;i++) {
+        for(int i=1;i<=4;i++) {
             Car car = new Car(cb);
             car.setName(i + "號車");
             car.start();
