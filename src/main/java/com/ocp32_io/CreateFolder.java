f51d5c75220c486f13d9ca432da7a66deb851435
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-02-04 19:14:10 +0800

    Java

--- src/main/java/com/ocp32_io/CreateFolder.java
+++ src/main/java/com/ocp32_io/CreateFolder.java
@@ -5,6 +5,13 @@ import java.io.File;
 // 建立資料夾 .../ocp32_io/files 資料夾
 public class CreateFolder {
     public static void main(String[] args) {
+        String path = "src\\main\\java\\com\\ocp32_io";
+        File folder = new File(path);
+        // 確認 folder 是否是資料夾 ?
+        if(folder.isDirectory()) {
         
+        } else {
+            System.out.printf("%s 不是資料夾\n", folder);
+        }
     }
 }
