a3b7a310f9b2a05fe3a12442a132cba9e404c3c6
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-02-09 19:28:48 +0800

    Java

--- src/main/java/food_salety/entity/Rice.java
+++ src/main/java/food_salety/entity/Rice.java
@@ -13,49 +13,94 @@ public class Rice {
     private String 行政處分;
     private String Log_UpdateTime;
 
+    public String getTitle() {
+        return Title;
+    }
+
     public void setTitle(String Title) {
         this.Title = Title;
+    }
+
+    public String get編號() {
+        return 編號;
     }
 
     public void set編號(String 編號) {
         this.編號 = 編號;
     }
 
+    public String get品名() {
+        return 品名;
+    }
+
     public void set品名(String 品名) {
         this.品名 = 品名;
+    }
+
+    public String get國際條碼() {
+        return 國際條碼;
     }
 
     public void set國際條碼(String 國際條碼) {
         this.國際條碼 = 國際條碼;
     }
 
+    public String get廠商名稱() {
+        return 廠商名稱;
+    }
+
     public void set廠商名稱(String 廠商名稱) {
         this.廠商名稱 = 廠商名稱;
+    }
+
+    public String get廠商地址() {
+        return 廠商地址;
     }
 
     public void set廠商地址(String 廠商地址) {
         this.廠商地址 = 廠商地址;
     }
 
+    public String get檢驗結果() {
+        return 檢驗結果;
+    }
+
     public void set檢驗結果(String 檢驗結果) {
         this.檢驗結果 = 檢驗結果;
+    }
+
+    public String get不合格原因() {
+        return 不合格原因;
     }
 
     public void set不合格原因(String 不合格原因) {
         this.不合格原因 = 不合格原因;
     }
 
+    public String get違反規定() {
+        return 違反規定;
+    }
+
     public void set違反規定(String 違反規定) {
         this.違反規定 = 違反規定;
+    }
+
+    public String get行政處分() {
+        return 行政處分;
     }
 
     public void set行政處分(String 行政處分) {
         this.行政處分 = 行政處分;
     }
 
+    public String getLog_UpdateTime() {
+        return Log_UpdateTime;
+    }
+
     public void setLog_UpdateTime(String Log_UpdateTime) {
         this.Log_UpdateTime = Log_UpdateTime;
     }
+
     
     
 }
--- src/test/java/food_salety/Test1.java
+++ src/test/java/food_salety/Test1.java
@@ -1,5 +1,6 @@
 package food_salety;
 
+import food_salety.entity.Rice;
 import food_salety.util.Service;
 import org.junit.Test;
 
@@ -11,5 +12,15 @@ public class Test1 {
         System.out.println(service.getJsonString(url));
         //String url2 = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceQualified.aspx";
         //System.out.println(service.getJsonString(url2));
+    }
+    
+    @Test
+    public void t2() {
+        Service service = new Service();
+        String url = "https://data.coa.gov.tw/Service/OpenData/FromM/AgricultureiRiceFailure.aspx";
+        Rice[] rices = service.queryRices(url);
+        for(Rice rice : rices) {
+            System.out.printf("%s %s\n", rice.get品名(), rice.get不合格原因());
+        }
     }
 }
