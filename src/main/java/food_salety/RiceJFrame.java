b3eef3f621cee4461f487a05956ef6069e2ea269
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-02-09 21:24:19 +0800

    Java

--- src/main/java/food_salety/RiceJFrame.java
+++ src/main/java/food_salety/RiceJFrame.java
@@ -141,7 +141,9 @@ public class RiceJFrame extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        
+        String keyword = kw.getText();
+        List<Rice> rices = service.queryRicesFromTable(keyword);
+        showRiceTable(rices);
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
