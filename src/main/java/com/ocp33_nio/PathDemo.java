d495ae96bf76f93613705d32142d5bfed6e86ff3
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-02-18 19:19:18 +0800

    Java

--- src/main/java/com/ocp33_nio/PathDemo.java
+++ src/main/java/com/ocp33_nio/PathDemo.java
@@ -5,7 +5,14 @@ import java.nio.file.Paths;
 
 public class PathDemo {
     public static void main(String[] args) {
-        Path path = Paths.get("C:\\Users\\MB-207\\Documents\\NetBeansProjects\\Java1103\\src\\main\\java\\com\\ocp33_nio\\PathDemo.java");
-        
+        Path path = Paths.get("src\\main\\java\\com\\ocp33_nio\\PathDemo.java");
+        System.out.printf("filename: %s\n", path.getFileName());
+        System.out.printf("parent: %s\n", path.getParent());
+        System.out.printf("root: %s\n", path.getRoot());
+        System.out.printf("namecount: %s\n", path.getNameCount());
+        System.out.printf("subpath(0, 3): %s\n", path.subpath(0, 3));
+        System.out.printf("isAbsolute: %s\n", path.isAbsolute());
+        System.out.printf("toAbsolutePath: %s\n", path.toAbsolutePath());
+        System.out.printf("toUri: %s\n", path.toUri());
     }
 }
