e42b4663dcf5495c8bfb95ae4be4c0e1f43d17fa
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-02-18 19:42:58 +0800

    Java

--- src/main/java/com/ocp33_nio/ReadAllLinesDemo.java
+++ src/main/java/com/ocp33_nio/ReadAllLinesDemo.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 public class ReadAllLinesDemo {
     public static void main(String[] args) {
@@ -21,5 +22,20 @@ public class ReadAllLinesDemo {
             br.lines().forEach(System.out::print);
         } catch (Exception e) {
         }
+        System.out.println();
+        // 3. Files.readAllLines (會得到一個 List 集合)
+        try {
+            List<String> list = Files.readAllLines(path);
+            list.forEach(System.out::print);
+        } catch (Exception e) {
+        }
+        System.out.println();
+        // 4. Files.lines
+        try {
+            Files.lines(path).forEach(System.out::print);
+        } catch (Exception e) {
+        }
+        
+        
     }
 }
