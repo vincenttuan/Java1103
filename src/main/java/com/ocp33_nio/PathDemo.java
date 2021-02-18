f48a45b7efc36113aa9c57fecce7bef21e3ad99c
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-02-18 19:13:32 +0800

    Java

---
+++ src/main/java/com/ocp33_nio/PathDemo.java
@@ -0,0 +1,11 @@
+package com.ocp33_nio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathDemo {
+    public static void main(String[] args) {
+        Path path = Paths.get("C:\\Users\\MB-207\\Documents\\NetBeansProjects\\Java1103\\src\\main\\java\\com\\ocp33_nio\\PathDemo.java");
+        
+    }
+}
