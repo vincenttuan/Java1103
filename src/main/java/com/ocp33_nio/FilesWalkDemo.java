2d02bb9ed987797bc42bebd7e6dc49e8e1621ef6
MB-teacher <MB-teacher@DESKTOP-KVQTFGR>
2021-02-18 20:59:47 +0800

    Java

--- src/main/java/com/ocp33_nio/FilesWalkDemo.java
+++ src/main/java/com/ocp33_nio/FilesWalkDemo.java
@@ -3,6 +3,7 @@ package com.ocp33_nio;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.stream.Stream;
 
 public class FilesWalkDemo {
@@ -10,7 +11,14 @@ public class FilesWalkDemo {
         // 尋訪家目錄下面的所有路徑與檔案(包含子目錄)
         // 並列出路徑/檔案名稱與建立時間
         Stream<Path> paths = Files.walk(Paths.get(System.getProperty("user.home")));
-        paths.forEach(f -> {});
+        paths.forEach(f -> {
+            try {
+                Path p = f.toAbsolutePath();
+                System.out.print(f + " : ");
+                System.out.println(Files.readAttributes(p, BasicFileAttributes.class).creationTime());
+            } catch (Exception e) {
+            }
+        });
         
     }
 }
